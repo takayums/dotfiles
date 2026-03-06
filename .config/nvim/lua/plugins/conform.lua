a42ff1123337a5d922e2ab7549e58c9c886d1841
takayums <takayums20@gmail.com>
2026-03-06 12:59:31 +0700

    nvim: mini-surround, tressitter prisma, and conform eslint, blank

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,28 @@
 return {
   "stevearc/conform.nvim",
-  opts = function()
-    local opts = {
-      default_format_opts = {
-        timeout_ms = 1000,
-        async = false,
-        quiet = false,
-        lsp_format = "fallback",
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        fish = { "fish_indent" },
-        sh = { "shfmt" },
-        python = { "black" },
-        html = { "prettier" },
-        css = { "prettier" },
-        json = { "prettier" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        vue = { "prettier" },
-        markdown = { "prettier" },
-        go = { "gofumpt", "goimports" },
-      },
-      format_on_save = true,
-    }
-    return opts
-  end,
+  opts = {
+    default_format_opts = {
+      timeout_ms = 2500,
+      async = false,
+      quiet = false,
+      lsp_format = "fallback",
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      sh = { "shfmt" },
+      python = { "black" },
+      html = { "prettier" },
+      css = { "prettier" },
+      json = { "prettier" },
+      prisma = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      vue = { "prettier" },
+      markdown = { "prettier" },
+      go = { "gofumpt", "goimports" },
+    },
+    format_on_save = true,
+  },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
         "vim",
         "yaml",
         "go",
+        "prisma",
       },
     },
   },
